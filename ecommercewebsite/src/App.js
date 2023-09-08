87ed069cf097b508c4b507e34f1fe663a3705707
Ayush Khadka <80869490+2ayush2@users.noreply.github.com>
2023-09-08 11:41:17 +0545

    updated

---
+++ ecommercewebsite/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil sunt
+      deserunt explicabo minima, iure quisquam a quos facilis debitis nisi
+      incidunt ab quae nesciunt sit. Possimus eligendi inventore necessitatibus
+      incidunt.
+    </div>
+  );
+}
+
+export default App;
